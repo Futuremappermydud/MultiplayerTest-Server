f34f5546a1696a98947ef460d617193b6edbdc73
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2021-01-08 11:49:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var server = require("http").Server(app);
 var io = require("socket.io")(server, {});
-var shortid = require('shortid');
 var app = express();
 
 var port = process.env.PORT || 7777
 
 app.use(express.static(__dirname));
-
 
 app.get("/", function(req, res) {
  res.render("index");
@@ -19,17 +17,5 @@ app.listen(port, function() {
 
 io.sockets.on("connection", function(socket)
 {
-    var clientName = shortid.generate();
-    console.log("client was connected, name = " + clientName);
- 
-    socket.on("getMyName", function()
-    {
-        socket.emit("onGetMyName", { name: clientName });
-    });
- 
-    socket.on("sendMyNameToAllClients", function()
-    {
-        socket.broadcast.emit("onSendMyNameToAllClients", { name: clientName });
-        console.log(clientName);
-    });
+    console.log("Connected");
 });
