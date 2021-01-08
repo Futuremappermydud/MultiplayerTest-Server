fc771d49960eb6ff8e731ff4230848e87377b5f7
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2021-01-08 14:46:58 -0500

    get ip

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ console.log("Started");
 app.listen(port, function() {
  console.log("running on " + port);
 })
+var http = require('http');
+
+
+var options = {
+  host: 'api.ipify.org',
+  path: '/?format=plain'
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been received, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been received, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request(options, callback).end();
