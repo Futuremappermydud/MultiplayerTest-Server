bc4005aa9a4d2885c6711aed7e37c512f8d99825
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2021-01-08 14:39:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var app = express();
 
 var port = process.env.PORT || 7777
-
+console.log("Started");
 app.listen(port, function() {
  console.log("running on " + port);
 })
