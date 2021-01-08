6a4705ebdb6917661873a40779fa75cadc48695f
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2021-01-08 14:35:46 -0500

    Update

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 var express = require("express");
-var server = require("http").Server(app);
-var io = require("socket.io")(server, {});
+var fs = require("fs");
 var app = express();
 
 var port = process.env.PORT || 7777
 
-app.use(express.static(__dirname));
-
-app.get("/", function(req, res) {
- res.render("index");
-})
-
 app.listen(port, function() {
  console.log("running on " + port);
 })
-
-io.sockets.on("connection", function(socket)
-{
-    console.log("Connected");
-});
